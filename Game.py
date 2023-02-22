8aab6a1fe614b7047c96f21eb092297d70bfbd01
Gavin Warnock <gbwarnock@gmail.com>
2023-02-22 13:41:41 -0700

    corrected issue with game logic and started work on multiplayer part

--- Game.py
+++ Game.py
@@ -31,6 +31,12 @@ class Game:
             p1_points = 0
             p2_points = 0
             while p1_points != 2 or p2_points != 2:
+                if p1_points >= 2:
+                    print("Player 1 WINS!")
+                    break
+                elif p2_points >= 2:
+                    print("Player 2 WINS!")
+                    break
                 print("Player one please choose your gesture!")
                 p1_choice = self.player_one.select_gesture()
                 p2_choice = self.player_two.ai_select_gesture()
@@ -43,12 +49,36 @@ class Game:
                         print("Player 1 has won the round!")
                     elif p2_choice == "Rock":
                         print("It's a tie!")
+                elif p1_choice == "Paper":
+                    if p2_choice == "Scissors" or p2_choice == "Lizard":
+                        print("Player 2 has won the round!")
+                        p2_points += 1
+                    elif p2_choice == "Rock" or p2_choice == "Spock":
+                        print("Player 1 has won the round!")
+                        p1_points += 1
+                elif p1_choice == "Scissors":
+                    if p2_choice == "Rock" or p2_choice == "Spock":
+                        print("Player 2 has won the round!")
+                        p2_points += 1
+                    elif p2_choice == "Paper" or p2_choice == "Lizard":
+                        print("Player 1 has won the round!")
+                        p1_points += 1
+                elif p1_choice == "Lizard":
+                    if p2_choice == "Rock" or p2_choice == "Scissors":
+                        print("Player 2 has won the round!")
+                        p2_points += 1
+                    elif p2_choice == "Paper" or p2_choice == "Spock":
+                        print("Player 1 has won the round!")
+                        p1_points += 1
+                elif p1_choice == "Spock":
+                    if p2_choice == "Paper" or p2_choice == "Lizard":
+                        print("Player 2 has won the round!")
+                        p2_points += 1
+                    elif p2_choice == "Rock" or p2_choice == "Scissors":
+                        print("Player 1 has won the round!")
+                        p1_points += 1        
                 
         else:
             "Please select 1 or 2"
-    
-            
-    
 
-
-
+        
